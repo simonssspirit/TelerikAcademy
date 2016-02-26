49ba90e27637197b10ac4d4039be56fc4a01e2d0
simonssspirit <simonssspirit@gmail.com>
2016-02-26 16:37:52 +0200

    C# part 1

---
+++ C#1/Console-Input-Output/NumbersDividableByN/NumbersDividableByN.cs
@@ -0,0 +1,28 @@
+﻿//Write a program that reads two positive 
+//integer numbers and prints how many numbers p exist 
+//between them such that the reminder of the division by 5 is 0.
+
+namespace NumbersDividableByN
+{
+    using System;
+    class NumbersDividableByN
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the first number: ");
+            int start = int.Parse(Console.ReadLine());
+            Console.Write("Enter the second number: ");
+            int end = int.Parse(Console.ReadLine());
+            int count = 0;
+            
+            if (end % 5 == 0 && start % 5 == 0)
+                ++count;
+
+            end /= 5;
+            start /= 5;
+            count += Math.Abs(end - start);
+
+            Console.WriteLine(count);
+        }
+    }
+}
