7b3e93b22773e7f7b2534694c68923ed3d4c97df
simonssspirit <simonssspirit@gmail.com>
2016-02-26 18:47:25 +0200

    Added files via upload

---
+++ WEB-SERVICES/MusicSystem/MusicSystem.Services/Models/AlbumDataModel.cs
@@ -0,0 +1,22 @@
+﻿namespace MusicSystem.Services.Models
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class AlbumDataModel
+    {
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public int Year { get; set; }
+
+        [Required]
+        public int ProducerId { get; set; }
+
+        [Required]
+        public int[] SongIds { get; set; }
+
+        [Required]
+        public int[] ArtistIds { get; set; }
+    }
+}
