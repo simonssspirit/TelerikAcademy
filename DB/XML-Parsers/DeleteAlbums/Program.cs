5e7e93bd722407c63875c9508f6d263a633991ab
simonssspirit <simonssspirit@gmail.com>
2016-02-26 18:22:45 +0200

    Added files via upload

---
+++ DB/XML-Parsers/DeleteAlbums/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.IO;
+
+namespace DeleteAlbums
+{
+    using System;
+    using System.Xml;
+
+    class Program
+    {
+        static void Main()
+        {
+            var doc = new XmlDocument();
+            doc.Load("../../../catalogue.xml");
+            var root = doc.DocumentElement;
+
+            DeleteAlbumsWithPrice(root, 20.0);
+            doc.Save("../../updatedCatalogue.xml");
+
+            var currentDir = Directory.GetCurrentDirectory();
+            var savedDir = currentDir.Substring(0, currentDir.IndexOf("bin\\Debug"));
+            Console.WriteLine("new catalogue saved as " + savedDir + "updatedCatalogue.xml");
+        }
+
+        private static void DeleteAlbumsWithPrice(XmlElement root, double maxPrice)
+        {
+            var childNodes = root.ChildNodes;
+
+            for (int a = childNodes.Count - 1; a >= 0; a--)
+            {
+                var album = childNodes[a];
+
+                var xmlPrice = album["price"].InnerText;
+                var price = double.Parse(xmlPrice);
+
+                if (price > maxPrice)
+                {
+                    root.RemoveChild(album);
+                }
+            }
+        }
+    }
+}
---
+++ DB/XML-Parsers/TextToXml/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace TextToXml
+{
+    using System;
+    using System.IO;
+    using System.Xml.Linq;
+
+    class Program
+    {
+        static void Main()
+        {
+            var person = new Person();
+
+            using (StreamReader reader = new StreamReader("../../person.txt"))
+            {
+                person.Name = reader.ReadLine();
+                person.Phone = reader.ReadLine();
+                person.Address = reader.ReadLine();
+            }
+
+            var personElement = new XElement("person", 
+                new XElement("name", person.Name), 
+                new XElement("phone", person.Phone), 
+                new XElement("address", person.Address));
+            
+            personElement.Save("../../person.xml");
+
+            var currentDir = Directory.GetCurrentDirectory();
+            var savedDir = currentDir.Substring(0, currentDir.IndexOf("bin\\Debug"));
+            Console.WriteLine("person saved as " + savedDir + "person.xml");
+        }
+    }
+}
