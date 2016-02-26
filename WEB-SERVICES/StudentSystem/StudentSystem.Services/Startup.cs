af0bf57cd02f544afdcdba733ae09dd18f5ed39c
simonssspirit <simonssspirit@gmail.com>
2016-02-26 18:48:33 +0200

    Added files via upload

---
+++ WEB-SERVICES/StudentSystem/StudentSystem.Services/Startup.cs
@@ -0,0 +1,24 @@
+﻿namespace StudentSystem.Services
+{
+    using System.Web.Http;
+
+    using AutoMapper;
+    using Models;
+    using Ninject.Web.Common.OwinHost;
+    using Ninject.Web.WebApi.OwinHost;
+    using Owin;
+    using StudentSystem.Models;
+
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            var config = new HttpConfiguration();
+            WebApiConfig.Register(config);
+            MappingConfig.RegisterMappings();
+
+            app.UseNinjectMiddleware(() => NinjectConfig.CreateKernel.Value);
+            app.UseNinjectWebApi(config);
+        }
+    }
+}
